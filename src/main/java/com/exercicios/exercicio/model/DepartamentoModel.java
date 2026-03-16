06eb4c2736c7f9343f45046b5769838c0ca09deb
gmiashiro <g.miashiro.freitas@gmail.com>
2026-03-16 19:52:40 -0300

    feat: setup inicial do projeto e camada model

---
+++ src/main/java/com/exercicios/exercicio/model/DepartamentoModel.java
@@ -0,0 +1,44 @@
+package com.exercicios.exercicio.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_departamento")
+public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeeee")
+    private String nome;
+    @Column(name = "local")
+    private String localizacao;
+
+    public DepartamentoModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
